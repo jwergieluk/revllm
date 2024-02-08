d8612bd544b75bcbbc97bc421cfa41f461be5f63
jwergieluk <297652+jwergieluk@users.noreply.github.com>
2024-02-08 08:14:32 +0000

    GPT code clarifications

--- revllm/gpt.py
+++ revllm/gpt.py
@@ -177,6 +177,37 @@ class GPT(nn.Module):
             if pn.endswith("c_proj.weight"):
                 torch.nn.init.normal_(p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer))
 
+    def forward(self, input_token_ids: torch.Tensor, targets=None):
+        device = input_token_ids.device
+        b, t = input_token_ids.size()
+        assert (
+            t <= self.config.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device)  # shape (t)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(input_token_ids)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos)  # position embeddings of shape (t, n_embd)
+        x = tok_emb + pos_emb
+        x = self.transformer.drop(x)
+
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is None:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(x[:, [-1], :])  # note: using list [-1] to preserve the time dim
+            loss = None
+            return logits, loss
+
+        # if we are given some desired targets also calculate the loss
+        logits = self.lm_head(x)
+        loss = F.cross_entropy(
+            logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+        )
+        return logits, loss
+
     def get_num_params(self, non_embedding: bool = True) -> int:
         """
         Return the number of parameters in the model.
@@ -202,36 +233,6 @@ class GPT(nn.Module):
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-
-    def forward(self, input_token_ids: torch.Tensor, targets=None):
-        device = input_token_ids.device
-        b, t = input_token_ids.size()
-        assert (
-            t <= self.config.block_size
-        ), f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
-        pos = torch.arange(0, t, dtype=torch.long, device=device)  # shape (t)
-
-        # forward the GPT model itself
-        tok_emb = self.transformer.wte(input_token_ids)  # token embeddings of shape (b, t, n_embd)
-        pos_emb = self.transformer.wpe(pos)  # position embeddings of shape (t, n_embd)
-        x = self.transformer.drop(tok_emb + pos_emb)
-
-        for block in self.transformer.h:
-            x = block(x)
-        x = self.transformer.ln_f(x)
-
-        if targets is not None:
-            # if we are given some desired targets also calculate the loss
-            logits = self.lm_head(x)
-            loss = F.cross_entropy(
-                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
-            )
-        else:
-            # inference-time mini-optimization: only forward the lm_head on the very last position
-            logits = self.lm_head(x[:, [-1], :])  # note: using list [-1] to preserve the time dim
-            loss = None
-
-        return logits, loss
 
     def forward_with_diagnostics(self, input_token_ids: torch.Tensor) -> GPTOutput:
         device = input_token_ids.device
