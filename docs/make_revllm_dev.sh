ad24645712f026b17de2d71f5c285d00f66661c5
jwergieluk <297652+jwergieluk@users.noreply.github.com>
2024-02-14 19:23:16 +0000

    Package website files

--- docs/make_revllm_dev.sh
+++ docs/make_revllm_dev.sh
@@ -3,3 +3,5 @@
 pandoc -f markdown -t html5 -s -o index.html revllm_dev.md
 pandoc -f markdown -t html5 -s -o imprint.html imprint.md
 pandoc -f markdown -t html5 -s -o privacy.html privacy.md
+
+zip -r revllm_dev.zip index.html imprint.html privacy.html *.png
